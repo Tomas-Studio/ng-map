547e4bbbe62d6159f2e9a46941d3af4f37c0713f
Tunji-Olakunle <olatunji.olakunle@cyphercrescent.com>
2022-10-28 18:24:58 +0100

    :rocket: initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import '@master/css';
+</script>
+
+<template>
+    <h1 class="font:40 font:heavy italic m:50 text:center">
+        Hello World
+    </h1>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
